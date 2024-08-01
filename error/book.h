534cc76071a8b8b0c3411e65c6420098d9ac1e2a
jylee1429 <123356726+jylee1429@users.noreply.github.com>
2024-08-01 14:44:34 +0900

    Add files via upload

---
+++ error/book.h
@@ -0,0 +1,51 @@
+#ifndef _BOOK_H_
+#define _BOOK_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class EachBook;
+
+class Book {
+protected:
+	string title; //제목
+	string author; //저자
+	string publisher; //출판사
+	vector<EachBook> each; //각각의 책 저장
+public:
+	Book();
+	//Book(int isbn, string title, string author, string publisher);
+
+	string getTitle() const;
+	string getAuthor() const;
+	string getPublisher() const;
+	int getBookISBN() const;
+
+	void addBook(int isbn, const string& title, const string& author, const string& publisher); //책 등록
+	void searchBook() const; //책 정보 출력
+	EachBook borrowBook(); //책 대출
+	void returnBook(int isbn); //책 반납
+
+};
+
+
+class EachBook : public Book {
+protected:
+	int ISBN;
+	bool borrow_status; //대출 여부
+
+public:
+	EachBook();
+	EachBook(int ISBN);
+
+	void setISBN(int isbn);
+	int getISBN() const;
+	bool available();
+
+	void borrow(int ISBN); //책 대출
+	void return_book(); //책 반납
+};
+#endif
---
+++ error/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#include "member.h"
+#include "book.h"
+
+using namespace std;
+
+vector<Book> book_list(3);
+
+int main(void) {
+	MemberList List;
+	
+	for (int i = 0; i < 3; i++) {
+		List.memberRegister();
+	}
+	
+	book_list[0].addBook(1234, "力格", "历磊", "免魄荤");
+	book_list[1].addBook(1235, "力格1", "历磊1", "免魄荤1");
+	book_list[2].addBook(1236, "力格2", "历磊2", "免魄荤2");
+	
+
+	for (int i = 0; i < 3; i++) {
+		// book_list[i].searchBook();
+	}
+
+	List.memberShow();
+
+	// List.memberBorrowBooks(1, "力格");
+	// List.memberBorrowBooks(1, "力格1");
+	// List.memberBorrowBooks(2, "力格2");
+
+	List.memberShow();
+
+
+	// List.memberShow();
+}
