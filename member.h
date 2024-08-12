eacfa0f90ab4b9735f0512fc013b2bc460c0a160
Yujin KIM <104256486+ky107601@users.noreply.github.com>
2024-08-12 16:12:38 +0900

    대출 함수 반환값 수정

--- member.h
+++ member.h
@@ -15,7 +15,7 @@ typedef map<int, shared_ptr<Member>> Member_maps;
 typedef map<int, shared_ptr<Book>> book_maps;
 
 class MemberManage {
-	Member_maps list;				// È¸¿ø Á¤º¸ ¸®½ºÆ® : <id, Member>
+	Member_maps list;				// íšŒì› ì •ë³´ ë¦¬ìŠ¤íŠ¸ : <id, Member>
 
 public:
     void memberRegister(int id, string name);
@@ -23,18 +23,18 @@ public:
 	void memberBorrowList(Member_maps::iterator& it);
 	void memberOverdueList(Member_maps::iterator& it);
 	void memberReturnBooks(vector<Book>& booklist, int id, int isbn);
-	void memberBorrowBooks(vector<Book>& book, int id, string title);
+    bool memberBorrowBooks(vector<Book>& book, int id, string title);
 	void showBoookList(vector<Book>& booklist) const;
         bool checkID(int id, string& name);
 };
 
 class Member {
 	int id;							//	id
-	int borrowed_cnt;				//	ÇöÀç ºô¸° Ã¥ ¼ö
-	int overdue_cnt;				//  ¹İ³³ÀÌ ¾ÈµÈ Ã¥ ¼ö
-	map<int, Book> overduebooks;	//  ¹Ì¹İ³³µÈ Ã¥ ¸®½ºÆ® : <ISBN, Book>
-	map<int, Book> borrowedbooks;	//  ºô¸° Ã¥ ¸®½ºÆ® : <ISBN, Book>
-	string name;					//  ÀÌ¸§
+	int borrowed_cnt;				//	í˜„ì¬ ë¹Œë¦° ì±… ìˆ˜
+	int overdue_cnt;				//  ë°˜ë‚©ì´ ì•ˆëœ ì±… ìˆ˜
+	map<int, Book> overduebooks;	//  ë¯¸ë°˜ë‚©ëœ ì±… ë¦¬ìŠ¤íŠ¸ : <ISBN, Book>
+	map<int, Book> borrowedbooks;	//  ë¹Œë¦° ì±… ë¦¬ìŠ¤íŠ¸ : <ISBN, Book>
+	string name;					//  ì´ë¦„
 
 public:
 	Member(int id = 0, int  borrowed_cnt = 0, int overdue_cnt = 0, string name = " ");
@@ -43,7 +43,7 @@ public:
 	int getBorrowedCnt(void) const;
 	int getOverdueCnt(void) const;
 	void returnBooks(vector<Book>& booklist, int isbn);
-	void borrowBooks(vector<Book>& book, string& title);
+    bool borrowBooks(vector<Book>& book, string& title);
 	void showOverdue(void) const;
 	void showBorrowed(void) const;
         QStringList checkReturnBook(int isbn);
